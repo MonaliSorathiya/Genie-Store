2318e822ff713c35c5d3103a6426b0e1fc6788dd
monalisorathiya <monalisorathiya8@gmail.com>
2024-03-02 21:56:12 +0530

    Updated prod url

--- angular-frontend/src/app/services/auth.service.ts
+++ angular-frontend/src/app/services/auth.service.ts
@@ -6,20 +6,20 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class AuthService {
-  baseUri: string = 'http://localhost:9091'; // For Local
-  // baseUri: string = 'http://webapp.geniestore.com'; // For Production
+  // baseURL: string = 'http://localhost:9091'; // For Local
+  baseURL: string = 'http://3.108.63.159:9091'; // For Production
 
   constructor(
     private http: HttpClient,
   ) { }
 
   registerNewUser(user: any) {
-    let url = `${this.baseUri}/api/userDetails/registerNewCustomer`;
+    let url = `${this.baseURL}/api/userDetails/registerNewCustomer`;
     console.log('url: registerURL', url);
     return this.http.post<any>(url, user);
   }
   login(auth: any) {
-    let url = `${this.baseUri}/api/userDetails/loginCustomer`;
+    let url = `${this.baseURL}/api/userDetails/loginCustomer`;
     console.log('url:loginURL ', url);
     return this.http.post<any>(url, auth);
   }
